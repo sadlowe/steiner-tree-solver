670a28ece222d907c6a7aee9cc5559a39b464bdb
Saad BENAQA <saad.benaqa@etu.univ-cotedazur.fr>
2026-01-23 16:31:06 +0100

    initial commit

---
+++ backend/src/main/java/com/terra/numerica/steiner_tree_solver/SteinerTreeSolverApplication.java
@@ -0,0 +1,13 @@
+package com.terra.numerica.steiner_tree_solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteinerTreeSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteinerTreeSolverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/terra/numerica/steiner_tree_solver/SteinerTreeSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terra.numerica.steiner_tree_solver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteinerTreeSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
